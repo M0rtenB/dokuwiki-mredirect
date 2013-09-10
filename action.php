89c6c078c240736afd91757d10d0c0126616e2e0
Morten Nielsen <mortenb@gmail.com>
2013-09-10 23:17:24 +0200

    first commit

---
+++ action.php
@@ -0,0 +1,36 @@
+<?php
+
+if(!defined('DOKU_INC')) die();
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(DOKU_PLUGIN.'action.php');
+
+class action_plugin_mredirect extends DokuWiki_Action_Plugin {
+
+    function register(&$controller){
+        $controller->register_hook('DOKUWIKI_STARTED', 'AFTER', $this, 'handle_start', array());
+    }
+
+    function handle_start(&$event, $param){
+        global $ID;
+        global $ACT;
+        global $INFO;
+
+      if ($ACT != 'show') return;
+      if (!($INFO['exists'])) return;          # don't try to read an article that doesn't exist
+      
+      $all = rtrim(rawWiki($ID)); $inner = substr ($all, 2, -2);
+      if (($all == '[[' . $inner . ']]') and (strpos ($inner, '[[') === false) and (strpos ($inner, ']]') === false)) {
+          if (!strpos ($inner, '://') === false) {
+              $url = $inner;    # link is URL already
+          } else {
+              msg (sprintf ('From: <a href="'.wl($ID,'do=edit').'">'.hsc($ID).'</a>'));
+              $url = html_wikilink ($inner, $name=null, $search='');
+              $url = substr ($url, strpos ($url, '"') + 1);
+              $url = substr ($url, 0, strpos ($url, '"'));
+          }
+          idx_addPage($ID);  # ensure fulltext search indexing of referrer article - to put it on the backlink page of target article
+          send_redirect($url);
+      }
+    }
+}
+?>
