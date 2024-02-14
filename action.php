2120ff9352492900a7588e1aeb29c22eadc71130
Demian Katz <demian.katz@villanova.edu>
2024-02-14 17:13:47 -0500

    Remove deprecated require_once.

--- action.php
+++ action.php
@@ -2,7 +2,6 @@
 
 if(!defined('DOKU_INC')) die();
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-require_once(DOKU_PLUGIN.'action.php');
 
 class action_plugin_mredirect extends DokuWiki_Action_Plugin {
 
